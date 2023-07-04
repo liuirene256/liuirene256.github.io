0af06d61660062003faef54ca8620407293eafe9
liuirene256 <71612595+liuirene256@users.noreply.github.com>
2023-07-04 10:54:11 -0500

    js update

--- scroll.js
+++ scroll.js
@@ -1,20 +1,6 @@
-function slide() {
-    const icon = document.querySelector(".menuicon");
-    const nav = document.querySelector(".navlinks");
-    const navLinks = document.querySelectorAll(".navlinks a");
-
-    icon.addEventListener("click", function() {
-        nav.classList.toggle("nav-active");
-
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s `;
-            }
-        });
-        icon.classList.toggle("toggle");
+$(window).load(function () {
+    $(".btn-nav").on("click tap", function () {
+        $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
+        $(this).toggleClass("animated");
     });
-};
-
-slide();
+});
