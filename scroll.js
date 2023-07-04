a7876f073b991aab43aef242a77267d6fd65d97d
liuirene256 <71612595+liuirene256@users.noreply.github.com>
2023-07-04 10:30:47 -0500

    menu js

--- scroll.js
+++ scroll.js
@@ -1,17 +1,16 @@
-const slide = () => {
+function slide() {
     const icon = document.querySelector(".menuicon");
     const nav = document.querySelector(".navlinks");
     const navLinks = document.querySelectorAll(".navlinks a");
 
-    icon.addEventListener("click", () => {
+    icon.addEventListener("click", function() {
         nav.classList.toggle("nav-active");
 
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5
-                    }s `;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s `;
             }
         });
         icon.classList.toggle("toggle");
