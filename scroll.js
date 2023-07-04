ed4e2f485ca986ea0d10f3fc64b0cd8ccd421289
liuirene256 <71612595+liuirene256@users.noreply.github.com>
2023-07-04 09:15:26 -0500

    js

--- scroll.js
+++ scroll.js
@@ -1,19 +1,4 @@
 window.onscroll = function () { stickyNav() };
-window.addEventListener("scroll", reveal);
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
 }
 
 var navbar = document.getElementById("navbar");
