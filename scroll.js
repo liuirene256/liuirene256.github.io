17aec0c25445e40fa695a0102c5e7ad9a57ec27b
liuirene256 <71612595+liuirene256@users.noreply.github.com>
2023-07-04 11:09:14 -0500

    u

--- scroll.js
+++ scroll.js
@@ -1,4 +1,4 @@
-$(window).load(function () {
+$(window).on('load', function () {
     $(".btn-nav").on("click tap", function () {
         $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
         $(this).toggleClass("animated");
