c58b391790dbeabad6620c027e725c288caacd07
liuirene256 <71612595+liuirene256@users.noreply.github.com>
2023-07-12 14:43:45 -0500

    finished index

--- scroll.js
+++ scroll.js
@@ -4,3 +4,30 @@ $(window).on('load', function () {
         $(this).toggleClass("animated");
     });
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+
+window.addEventListener("load", reveal);
